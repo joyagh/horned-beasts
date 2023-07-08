368c440d288c7e4f49873c8214754e96734e97e7
Justine Oyaghiro <justineoyaghiro@icloud.com>
2023-07-08 14:17:32 -0500

    imported footer

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 class Person extends React.Component {
-render() {
-     let name = Justine
-     return(
-          <div>
-               <h2>{ name }</h2>
-          </div>
-     )
-}
+  render() {
+    let name = "Justine";
+
+    return (
+      <div>
+        <h2>{name}</h2>
+      </div>
+    );
+  }
 }
 
-export default Person
+export default Person;
