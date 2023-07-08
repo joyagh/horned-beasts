4a08e22260581c4ec9688b3fa25cca717e3a0a59
Justine Oyaghiro <justineoyaghiro@icloud.com>
2023-07-08 14:11:38 -0500

    added main, header, and footer components

---
+++ src/components/Person.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Person extends React.Component {
+render() {
+     let name = Justine
+     return(
+          <div>
+               <h2>{ name }</h2>
+          </div>
+     )
+}
+}
+
+export default Person
